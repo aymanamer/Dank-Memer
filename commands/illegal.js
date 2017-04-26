3508dca560c2ad8592563041b79b89132cf4f363
melmsie <memyselfandlie@gmail.com>
2017-04-26 09:18:40 -0400

    added api key

--- commands/illegal.js
+++ commands/illegal.js
@@ -1,6 +1,12 @@
 exports.run = function (client, msg, args) {
 
 const needle = require('needle');
+
+let options = {
+        headers: {
+            "Api-Key": "192aa6cf36a84f69ac3f0435b8676ddf"
+        }
+    }
     needle.get(`https://martmists.com/api/v1/illegal?query=${args.join(' ')}`, (err, res) => {
         if (err) return msg.channel.sendMessage('Congress stopped that from becoming illegal :(');
         msg.channel.sendFile(res.body, "illegal.gif")
--- commands/magik.js
+++ commands/magik.js
@@ -1,6 +1,12 @@
 exports.run = function (client, msg, args) {
 let avatarurl = msg.mentions.users.size > 0 ? msg.mentions.users.first().displayAvatarURL.replace("gif", "png") : msg.author.displayAvatarURL.replace("gif", "png")
 const needle = require('needle')
+
+let options = {
+        headers: {
+            "Api-Key": "192aa6cf36a84f69ac3f0435b8676ddf"
+        }
+    }
     needle.get(`https://martmists.com/api/v1/magik?url=${avatarurl}`, (err, res) => {
         if (err) return msg.channel.sendMessage('You just aren\'t magik enough :(')
         msg.channel.sendFile(res.body, "magik.png")
