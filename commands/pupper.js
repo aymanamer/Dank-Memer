a19004f4257aca3b44ba0a8b156b0388d56d52ba
Austin Melms <memyselfandlie@gmail.com>
2017-05-30 21:20:28 -0400

    g

--- commands/pupper.js
+++ commands/pupper.js
@@ -1,15 +1,7 @@
 const superagent = require("superagent");
 
-const { randomInArray } = require('../utils')
-const { pup } = require('../assets/arrays.json')
+
 
 exports.run = async function (client, msg) {
-	const notice = await msg.channel.send(randomInArray(pup))
-	let pupper = await superagent.get("http://random.dog/woof")
-
-	setTimeout(() => {
-		msg.channel.sendFile("http://random.dog/" + pupper.text).then(() => {
-			notice.delete()
-		})
-	}, 2000)
+	msg.channel.send('This command is being deleted')
 }
