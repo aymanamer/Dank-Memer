40645fa140c045da6eab0fdf23ad06d4b89829ff
Austin Melms <memyselfandlie@gmail.com>
2017-06-03 03:16:16 -0400

    g

--- commands/stop.js
+++ commands/stop.js
@@ -1,9 +1,7 @@
 exports.run = function (client, msg, args) {
     if (client.voiceConnections.get(msg.guild.id)) {
-        if (msg.author.voiceChannel) {
-            client.voiceConnections.get(msg.guild.id).channel.leave()
-            msg.react('😢')
-        }
+        client.voiceConnections.get(msg.guild.id).channel.leave()
+        msg.react('😢')
     } else {
         return msg.channel.send('I\'m not even in a voice channel <:waitwhat:320387072290455554> ')
     }
