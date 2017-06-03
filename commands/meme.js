5e0957d9de8fb7a0b8b7a64fed77a3027fd7a82c
Austin Melms <memyselfandlie@gmail.com>
2017-06-03 04:01:33 -0400

    ty

--- commands/meme.js
+++ commands/meme.js
@@ -1,11 +1,12 @@
 const { meme } = require('../assets/arrays.json')
 const { randomInArray } = require('../utils')
 
-if (!msg.channel.permissionsFor(client.user.id).has("ATTACH_FILES")) {
-    return msg.author.send('I don\'t have permission to send pictures in #' + msg.channel.name)
-}
-
 exports.run = function (client, msg) {
+
+    if (!msg.channel.permissionsFor(client.user.id).has("ATTACH_FILES")) {
+        return msg.author.send('I don\'t have permission to send pictures in #' + msg.channel.name)
+    }
+
     try {
         msg.channel.send({
             files: [randomInArray(meme)]
