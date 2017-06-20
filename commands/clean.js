90a3b83b284c144c76c71bce579d18bd97c4c7bc
Aetheryx <zain1@live.nl>
2017-06-21 01:57:41 +0200

    Uses await, way shorter and cleaner

--- commands/clean.js
+++ commands/clean.js
@@ -1,19 +1,9 @@
-exports.run = function (client, msg) {
-    if (!msg.channel.permissionsFor(client.user.id).has("MANAGE_MESSAGES")) {
-        return msg.author.send('I don\'t have permission to manage messages in #' + msg.channel.name).catch(console.error)
-    }
+exports.run = async function (client, msg) {
+	if (!msg.channel.permissionsFor(msg.author.id).has('MANAGE_MESSAGES'))
+		return msg.author.send('You don\'t have permission to send manage messages #' + msg.channel.name).catch(console.error)
 
-    if (!msg.channel.permissionsFor(msg.author.id).has("MANAGE_MESSAGES")) {
-        return msg.author.send('You don\'t have permission to send manage messages #' + msg.channel.name).catch(console.error)
-    }
-
-    let num = parseInt(10)
-    msg.channel.fetchMessages({
-        limit: 100
-    }).then(msgs => {
-        let mga = msgs.array()
-        mga = mga.filter(m => m.author.id === client.user.id)
-        mga.length = num
-        mga.map(m => m.delete().catch())
-    })
+	let messages = await msg.channel.fetchMessages({ limit: 100 })
+	messages = messages.array().filter(m => m.author.id === client.user.id)
+	messages.length = 10
+	messages.forEach(m => m.delete().catch())
 }
