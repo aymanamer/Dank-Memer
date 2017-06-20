265cea34a6e871aada56ff2e5c056050cd20d5b6
Aetheryx <zain1@live.nl>
2017-06-21 01:58:28 +0200

    Small change

--- commands/joke.js
+++ commands/joke.js
@@ -1,25 +1,21 @@
 const snekfetch = require('snekfetch')
-let color = [0x7d5bbe, 0xa3d3fe, 0x333333, 0x007acc, 0xf56154, 0xdc3522]
+const color = [0x7d5bbe, 0xa3d3fe, 0x333333, 0x007acc, 0xf56154,  0xdc3522] 
 
 exports.run = async function (client, msg) {
-    const res = await snekfetch.get('https://www.reddit.com/r/Jokes/top/.json?sort=top&t=day&limit=400')
-    let posts = res.body.data.children;
+	const res = await snekfetch.get('https://www.reddit.com/r/Jokes/top/.json?sort=top&t=day&limit=400')
+	let posts = res.body.data.children.filter(post => !post.data.preview && post.data.selftext.length <= 550 && post.data.title.length <= 256)
 
-    posts = posts
-        .filter(post => !post.data.preview && post.data.selftext.length <= 550 && post.data.title.length <= 256)
+	if (!client.indexes.joke[msg.guild.id] || client.indexes.joke[msg.guild.id] >= posts.length)
+		client.indexes.joke[msg.guild.id] = 1
 
-    if (!client.indexes.joke[msg.guild.id] || client.indexes.joke[msg.guild.id] >= posts.length)
-        client.indexes.joke[msg.guild.id] = 1
-
-    let post = posts[client.indexes.joke[msg.guild.id]]
-    client.indexes.joke[msg.guild.id]++
-    msg.channel.send({
-        embed: {
-            title: post.data.title,
-            color: color[Math.floor(Math.random() * color.length)],
-            url: post.data.url,
-            description: post.data.selftext,
-            footer: { text: `posted by ${post.data.author}` }
-        }
-    })
+	let post = posts[client.indexes.joke[msg.guild.id]]
+	client.indexes.joke[msg.guild.id]++
+	
+	msg.channel.send({ embed: {
+		title: post.data.title,
+		color: color[Math.floor(Math.random() *color.length)],
+		url: post.data.url,
+		description: post.data.selftext,
+		footer: { text: `posted by ${post.data.author}` }
+	}})
 }
