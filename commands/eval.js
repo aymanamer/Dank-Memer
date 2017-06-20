c0a43565340f4aa925f07d43319758676e891626
Aetheryx <zain1@live.nl>
2017-06-21 01:47:26 +0200

    Prettier eval command

--- commands/eval.js
+++ commands/eval.js
@@ -1,30 +1,21 @@
-const util = require("util")
-exports.run = function (client, msg, args, config, Discord) {
+exports.run = function (client, msg, args) {
 	if (msg.author.id !== config.owner) return
+	let res
 	try {
 		let before = Date.now()
-		let rep = new RegExp(client.user.email + "|" + client.token, "gi");
-		let code = eval(args.join(" "));
-		if (typeof code === "string") code = code.replace(rep, "*");
-		else code = util.inspect(code, {
-			depth: 0
-		}).replace(rep, "*");
+		let rep = new RegExp(client.user.email + '|' + client.token, 'gi')
+		res = eval(args.join(' '))
+		if (typeof res === 'string') res = res.replace(rep, '*')
+		else res = util.inspect(res, { depth: 0 }).replace(rep, '*')
 		let evalTime = Date.now() - before
-		msg.channel.send({
-			embed: new Discord.RichEmbed()
-				.setColor("#7d5bbe")
-				.setFooter(`evaluated in ${evalTime}ms`)
-				.addField("Input", `\`\`\`js\n${args.join(' ')}\`\`\``)
-				.addField("Output", `\`\`\`js\n${code}\`\`\``)
-
-		})
-	} catch (e) {
-		msg.channel.send({
-			embed: new Discord.RichEmbed()
-				.setColor("#7d5bbe")
-				.addField("Input", `\`\`\`js\n${args.join(' ')}\`\`\``)
-				.addField("Output", `\`\`\`js\n${e}\`\`\``)
-
-		})
+	} catch (err) {
+		res = err
 	}
+	msg.channel.send({
+		embed: new Discord.RichEmbed()
+			.setColor('#7d5bbe')
+			.addField('Input', `\`\`\`js\n${args.join(' ')}\`\`\``)
+			.addField('Output', `\`\`\`js\n${res}\`\`\``)
+			.setFooter(`evaluated in ${evalTime}ms`)
+	})
 }
