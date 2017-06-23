90c6ca410c18717606dd5d4ec656de9cb4f10391
Aetheryx <zain1@live.nl>
2017-06-23 19:39:39 +0200

    Now uses collectors for confirmation

--- commands/tweet.js
+++ commands/tweet.js
@@ -9,19 +9,35 @@ const tClient = new twit({
 
 exports.run = function (client, msg, args, undefined, Discord) {
 	args = args.join(' ')
+	if (args.length < 1)
+		return msg.channel.send('What do you want me to tweet?')
 	if (args.length > 140)
 		return msg.channel.send(`Tweet too long. You're ${args.length - 140} characters over the limit!`)
-	tClient.post('statuses/update', { status: args }, (err, data, response) => {
-		if (err)
-			return msg.channel.send('Something went wrong. \n' + err.stack)
-		if (response.statusCode !== 200)
-			return msg.channel.send('Something went wrong. Please try again later.')
-		msg.channel.send({
-			embed: new Discord.RichEmbed()
-				.setColor('#7d5bbe')
-				.setTitle(`Tweet Sent!`)
-				.setDescription(`[View here](https://twitter.com/${data.user.screen_name}/status/${data.id_str}) `)
-				.setFooter('See this tweet, and more @plsmeme')
-		})
+
+	msg.channel.send(`Are you sure you want to tweet \`${args}\`? You will be **permanently banned** from using Dank Memer for bad tweets. Answer with \`yes\`/\`no\`.`)
+	const collector = msg.channel.createMessageCollector(m => msg.author.id === m.author.id, { time: 40000 })
+	collector.on('collect', (m) => {
+		if (m.content.toLowerCase() === 'yes') {
+			tClient.post('statuses/update', { status: args }, (err, data, response) => {
+				if (err)
+					return msg.channel.send('Something went wrong. \n' + err.stack)
+				if (response.statusCode !== 200)
+					return msg.channel.send('Something went wrong. Please try again later.')
+				msg.channel.send({
+					embed: new Discord.RichEmbed()
+						.setColor('#7d5bbe')
+						.setTitle('Tweet Sent!')
+						.setDescription(`[View here](https://twitter.com/${data.user.screen_name}/status/${data.id_str}) `)
+						.setFooter('See this tweet, and more @plsmeme')
+				})
+			})
+		} else {
+			msg.channel.send('Good. Watching you :eyes:')
+		}
+		return collector.stop()
+	})
+	collector.on('end', (collected, reason) => {
+		if (reason === 'time') 
+			msg.channel.send('Prompt timed out.')
 	})
 }
