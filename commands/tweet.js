3e234ee63972e5ffaba678e05616a11471188854
Aetheryx <zain1@live.nl>
2017-06-23 19:13:45 +0200

    Finished tweet.js

--- commands/tweet.js
+++ commands/tweet.js
@@ -1,9 +1,17 @@
-exports.run = function (client, msg, args, config, Discord) {
+const twit = require('twit')
+const tClient = new twit({
+	consumer_key:         'Gkan9QvKDjZgWnJajCPMZ8jxL',
+	consumer_secret:      '5x3EkR48doQGXxlrEG2LLvWvemE9We20TlW6dgabC7zRUiScxS',
+	access_token:         '878224959151247361-0sxlyNs1WxVNcsZQmrspT3sWjUnPd1x',
+	access_token_secret:  'nlZbvOYEnlN4vqlcB1Ips5c2qT9suL1KXPRDyDZxaPpsL',
+	timeout_ms:           60*1000,
+})
+
+exports.run = function (client, msg, args, undefined, Discord) {
+	args = args.join(' ')
 	if (args.length > 140)
 		return msg.channel.send(`Tweet too long. You're ${args.length - 140} characters over the limit!`)
-	tClient.post('statuses/update', {
-		status: args
-	}, (err, data, response) => {
+	tClient.post('statuses/update', { status: args }, (err, data, response) => {
 		if (err)
 			return msg.channel.send('Something went wrong. \n' + err.stack)
 		if (response.statusCode !== 200)
@@ -17,11 +25,3 @@ exports.run = function (client, msg, args, config, Discord) {
 		})
 	})
 }
-
-/*
-	Consumer Key (API Key)	Gkan9QvKDjZgWnJajCPMZ8jxL
-	Consumer Secret (API Secret)	5x3EkR48doQGXxlrEG2LLvWvemE9We20TlW6dgabC7zRUiScxS
-
-	Access Token	878224959151247361-0sxlyNs1WxVNcsZQmrspT3sWjUnPd1x
-	Access Token Secret	nlZbvOYEnlN4vqlcB1Ips5c2qT9suL1KXPRDyDZxaPpsL
-*/
