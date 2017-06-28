f5e75d5b42b3a11635af13609575265bdd069763
melmsie <memyselfandlie@gmail.com>
2017-06-28 15:12:52 -0400

    stop

--- commands/stop.js
+++ commands/stop.js
@@ -1,12 +1,10 @@
 exports.run = async function (client, msg) {
 
-	if (!msg.member.voiceChannel) 
-		return msg.channel.send('You\'re not even in a voice channel <:waitwhat:320387072290455554>')
-	
-	if (client.voiceConnections.get(msg.guild.id)) {
-		client.voiceConnections.get(msg.guild.id).channel.leave()
-		msg.react('😢')
-	} else {
-		return msg.channel.send('I\'m not even in a voice channel <:waitwhat:320387072290455554>')
-	}
+	if (!client.voiceConnections.get(msg.guild.id)) return
+	if (!msg.member.voiceChannel) return msg.channel.send('You\'re not even in a voice channel <:waitwhat:320387072290455554>')
+	if (msg.member.voiceChannel.id != client.voiceConnections.get(msg.guild.id).channel.id) return msg.channel.send('You\'re not even in my voice channel <:waitwhat:320387072290455554>')
+
+	client.voiceConnections.get(msg.guild.id).channel.leave()
+	msg.react('😢')
+
 }
