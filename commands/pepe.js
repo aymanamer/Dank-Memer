e1db8c90a5c53537dd76c39cbbd8264ce01d086d
Austin Melms <memyselfandlie@gmail.com>
2017-07-01 18:55:12 -0400

    ssadfsdf

--- commands/pepe.js
+++ commands/pepe.js
@@ -2,7 +2,8 @@ exports.run = function (client, msg) {
 	if (!msg.channel.permissionsFor(client.user.id).has('ATTACH_FILES'))
 		return msg.author.send(`I don't have permission to send pictures in #${msg.channel.name}`)
 
-	if (!client.ids.donors.donor1.includes(msg.author.id))
+	if (!client.ids.donors.donor1.concat(client.ids.donors.donor5, client.ids.donors.donor10).includes(msg.author.id))
+
 		return msg.reply('to access this command, you must donate at the $10 tier here: <https://www.patreon.com/melmsie>')
 
 	msg.channel.send({ files: ['http://vignette3.wikia.nocookie.net/cookietest/images/e/e2/FeelsBadMan.png'] })
