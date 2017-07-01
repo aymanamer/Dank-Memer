4377d312ce5e3a81105fc8fa9d5a0ffe8c25f4b7
Aetheryx <zain1@live.nl>
2017-07-01 02:31:41 +0200

    Cleaned up, moved cpuAv function outside, checks proper ID source now

--- commands/debug.js
+++ commands/debug.js
@@ -1,60 +1,14 @@
-const bl = require('../blacklist.json')
 const os = require('os')
 exports.run = async function (client, msg, args, config, Discord) {
-
-
-	//Create function to get CPU information
-	function cpuAverage() {
-
-		//Initialise sum of idle and time of cores and fetch CPU info
-		var totalIdle = 0,
-			totalTick = 0
-		var cpus = os.cpus()
-
-		//Loop through CPU cores
-		for (var i = 0, len = cpus.length; i < len; i++) {
-
-			//Select CPU core
-			var cpu = cpus[i]
-
-			//Total up the time in the cores tick
-			for (type in cpu.times)
-				totalTick += cpu.times[type]
-
-
-			//Total up the idle time of the core
-			totalIdle += cpu.times.idle
-		}
-
-		//Return the average Idle and Tick times
-		return {
-			idle: totalIdle / cpus.length,
-			total: totalTick / cpus.length
-		}
-	}
-
-	//Grab first CPU Measure
-	var startMeasure = cpuAverage()
-
-	//Set delay for second Measure
-	setTimeout(async function () {
-
-		//Grab second Measure
-		var endMeasure = cpuAverage()
-
-		//Calculate the difference in idle and total time between the measures
-		var idleDifference = endMeasure.idle - startMeasure.idle
-		var totalDifference = endMeasure.total - startMeasure.total
-
-		//Calculate the average percentage CPU usage
-		var percentageCPU = 100 - ~~(100 * idleDifference / totalDifference)
-
-		//Output result to console
-
-
+	const startMeasure = cpuAverage()
+	setTimeout(async () => {
+		const endMeasure      = cpuAverage()
+		const idleDifference  = endMeasure.idle - startMeasure.idle
+		const totalDifference = endMeasure.total - startMeasure.total
+		const percentageCPU   = 100 - ~~(100 * idleDifference / totalDifference)
 		const guilds = (await client.shard.fetchClientValues('guilds.size')).reduce((a, b) => a + b)
-		const vcs = (await client.shard.fetchClientValues('voiceConnections.size')).reduce((a, b) => a + b)
-		const users = (await client.shard.fetchClientValues('users.size')).reduce((a, b) => a + b)
+		const users  = (await client.shard.fetchClientValues('users.size')).reduce((a, b) => a + b)
+		const vcs    = (await client.shard.fetchClientValues('voiceConnections.size')).reduce((a, b) => a + b)
 
 		await msg.channel.send({
 			embed: new Discord.RichEmbed()
@@ -62,31 +16,39 @@ exports.run = async function (client, msg, args, config, Discord) {
 				.setTitle(`${client.user.username} - Stats (${config.version})`)
 				.setDescription(`Uptime: ${timeCon(process.uptime())}`)
 				.addField('Websocket Ping', `${client.ping.toFixed(0)} ms`, true)
-				.addField(`CPU ${percentageCPU}% Usage`, true)
+				.addField('CPU Usage', `${percentageCPU}% Usage`, true)
 				.addField('RAM Usage', `RSS: ${(process.memoryUsage().rss / 1048576).toFixed()}MB\nHeap: ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)}MB`, true)
 				.addField('Total Servers', guilds, true)
 				.addField('Total Users', users, true)
 				.addField('Active VCs', vcs, true)
-				.addField('Blacklisted Users', bl.people.length, true)
-				.addField('Blacklisted Servers', bl.servers.length, true)
-				.addField('Donor Count', config.donor1.length + config.donor5.length + config.donor10.length, true)
-
-
+				.addField('Blacklisted Users', client.ids.blocked.user.length, true)
+				.addField('Blacklisted Servers', client.ids.blocked.guild.length, true)
+				.addField('Donor Count', client.ids.donors.donor1.length + client.ids.donors.donor5.length + client.ids.donors.donor10.length, true)
 		})
-
-
 	}, 100)
+}
 
-
-
-
+function cpuAverage () {
+	let totalIdle = 0,
+		totalTick = 0
+	const cpus = os.cpus()
+	for (var i = 0, len = cpus.length; i < len; i++) {
+		var cpu = cpus[i]
+		for (const type in cpu.times)
+			totalTick += cpu.times[type]
+		totalIdle += cpu.times.idle
+	}
+	return {
+		idle: totalIdle / cpus.length,
+		total: totalTick / cpus.length
+	}
 }
 
 function timeCon (time) {
-	let days = Math.floor((time % 31536000) / 86400)
-	let hours = Math.floor(((time % 31536000) % 86400) / 3600)
-	let minutes = Math.floor((((time % 31536000) % 86400) % 3600) / 60)
-	let seconds = Math.round((((time % 31536000) % 86400) % 3600) % 60)
+	let days = Math.floor(time % 31536000 / 86400)
+	let hours = Math.floor(time % 31536000 % 86400 / 3600)
+	let minutes = Math.floor(time % 31536000 % 86400 % 3600 / 60)
+	let seconds = Math.round(time % 31536000 % 86400 % 3600 % 60)
 	days = days > 9 ? days : days
 	hours = hours > 9 ? hours : hours
 	minutes = minutes > 9 ? minutes : minutes
