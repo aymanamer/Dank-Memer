f2fb9c79a4e6154c9b409652535dc745b61cdc00
Austin Melms <memyselfandlie@gmail.com>
2017-07-01 18:14:13 -0400

    sdfsdfsdf
    sdfsdfd

--- commands/patreon.js
+++ commands/patreon.js
@@ -4,7 +4,7 @@ exports.run = function (client, msg, args, config, Discord) {
 			.setColor('#3676b3')
 			.setURL('https://www.patreon.com/melmsie')
 			.setTitle('Donate to Dank Memer on Patreon!')
-			.setDescription('**__Premium Key Contents:__**\n❯ *75% reduced cooldowns on all commands*\n❯ *Donor role on Meme Central*\n❯ *Access to pls repeat*\n❯ *Guaranteed faster support*\n❯ *The opportunity to help test new commands*')
+			.setDescription('**__Premium Key Contents:__**\n❯ *75% reduced cooldowns on all commands*\n❯ *Donor role on Meme Central*\n❯ *Access to pls repeat*\n❯ *Guaranteed faster support*\n❯ *The opportunity to help test new commands*\n❯ *Access to donor image tags*')
 			.addField('$2 Tier <:feelsbadman:326155536867393536>', '❯ 1 Premium Key')
 			.addField('$5 Tier <:feelscuteman:325733304072667146>', '❯ 1 Premium Key\n❯ Access to pls spam\n❯ pls dank will generate a gif for you')
 			.addField('$10 Tier <:feelskawaiiman:326155537073045504>', '❯ 2 Premium Keys (Give one to a friend!)\n❯ Access to pls spam\n❯ pls dank will generate a gif for you\n❯ 1 custom command')
--- commands/troll.js
+++ commands/troll.js
@@ -1,7 +1,7 @@
 exports.run = function (client, msg) {
 	if (!msg.channel.permissionsFor(client.user.id).has('ATTACH_FILES'))
 		return msg.author.send(`I don't have permission to send pictures in #${msg.channel.name}`)
-	if (!client.ids.donors.donor10.includes(msg.author.id))
+	if (!client.ids.donors.donor1.includes(msg.author.id))
 		return msg.reply('to access this command, you must donate at the $10 tier here: <https://www.patreon.com/melmsie>')
 
 	msg.channel.send({ files: ['http://www.stickpng.com/assets/images/580b585b2edbce24c47b2a27.png'] })
