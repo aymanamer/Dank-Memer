ef60aad1cb7874d224a61023317462d5c0d46b0a
Aetheryx <zain1@live.nl>
2017-07-01 02:16:39 +0200

    Delete commands moved to dev.js

--- commands/reboot.js
+++
@@ -1,8 +0,0 @@
-exports.run = async function (client, msg, args, config) {
-	if (msg.author.id !== config.owner) 
-		return msg.channel.send('tfw you don\'t have permission to use this command :fire:')
-
-	await msg.react('👌')
-	await msg.channel.send('Rebooting...')
-	process.exit()
-}
