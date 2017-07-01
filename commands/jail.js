d284a2a1353c299b1de641ed94b48e75f11d5ad8
Aetheryx <zain1@live.nl>
2017-07-01 02:31:14 +0200

    Cleaned up image commands

--- commands/jail.js
+++ commands/jail.js
@@ -1,28 +1,25 @@
 const snakefetch = require('snekfetch')
 
-exports.run = async function (client, msg, args) {
+exports.run = async function (client, msg) {
 	msg.channel.startTyping()
 
-	let avatarurl = (msg.mentions.users.size > 0 ? msg.mentions.users.first().displayAvatarURL : msg.author.displayAvatarURL).replace('gif', 'png')
-
-	let data = await snakefetch
-		.get('http://www.get-ur-me.me/api/jail')
-		.set('Api-Key', 'XfGC62d9xKiOc4IegPdz')
-		.set('data-src', avatarurl)
+	const avatarurl = (msg.mentions.users.size > 0 ? msg.mentions.users.first().displayAvatarURL : msg.author.displayAvatarURL).replace('gif', 'png')
+	const data = await snakefetch
+        .get('http://www.get-ur-me.me/api/invert')
+        .set('Api-Key', 'XfGC62d9xKiOc4IegPdz')
+        .set('data-src', avatarurl)
 
 	if (data.status === 200) {
-		msg.channel.send({
+		const m = await msg.channel.send({
 			files: [{
 				name: 'jail.png',
 				attachment: data.body
 			}]
-		}).then(m => {
-			client.shard.broadcastEval(`const { RichEmbed } = require('discord.js')\nthis.channels.has('329799125015199744') && this.channels.get('329799125015199744').send({ embed: new RichEmbed().setAuthor('${msg.author.tag}').setImage('${m.attachments.first().url}') .addField('Sent from:', '#${msg.channel.name} in ${msg.guild.name}').setColor('#00c853')})`)
-			msg.channel.stopTyping()
 		})
+		client.shard.broadcastEval(`const { RichEmbed } = require('discord.js')\nthis.channels.has('329799125015199744') && this.channels.get('329799125015199744').send({ embed: new RichEmbed().setAuthor('${msg.author.tag}').setImage('${m.attachments.first().url}') .addField('Sent from:', '#${msg.channel.name} in ${msg.guild.name}').setColor('#00c853')})`)
 	} else {
-		msg.channel.send('Error: ' + data.text)
-		msg.channel.stopTyping()
+		msg.channel.send(`Error: ${data.text}`)
 	}
+	msg.channel.stopTyping()
 }
 
