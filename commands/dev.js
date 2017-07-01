ef260b5834966036d7025dfa8ac960f6de4e83c8
Austin Melms <memyselfandlie@gmail.com>
2017-06-30 22:40:15 -0400

    sddfds

--- commands/dev.js
+++ commands/dev.js
@@ -8,9 +8,8 @@ const fs = require('fs')
 exports.run = async function (client, msg, args, config, Discord) {
 	if (!config.devs.includes(msg.author.id)) return
 
-	const command = args[0].toLowerCase()
 
-	args.shift()
+
 
 	if (command === 'help' || !args[0])
 		msg.channel.send({
@@ -25,6 +24,9 @@ exports.run = async function (client, msg, args, config, Discord) {
 				.addField('blacklist', '[add, remove] [guild, user, channel] <id or @tag>')
 				.setDescription('Now go fuck people up with these OP commands!')
 		})
+
+	const command = args[0].toLowerCase()
+	args.shift()
 
 	if (command === 'reboot')
 		if (args[0] === 'shard') {
