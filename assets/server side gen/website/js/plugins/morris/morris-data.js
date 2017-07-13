3c8eb8ff56fdfb2f7d8fdbab341c8fa8b37fa241
Austin Melms <memyselfandlie@gmail.com>
2017-07-13 10:03:18 -0400

    hmm?

--- assets/server side gen/website/js/plugins/morris/morris-data.js
+++ assets/server side gen/website/js/plugins/morris/morris-data.js
@@ -1,64 +1,62 @@
 // Morris.js Charts sample data for SB Admin template
 
-$(function() {
+$(function () {
 
     // Area Chart
     Morris.Area({
         element: 'morris-area-chart',
         data: [{
-            period: '2010 Q1',
-            iphone: 2666,
-            ipad: null,
-            itouch: 2647
+            period: '12am',
+            RAM: 69,
+            CPU: 42
         }, {
-            period: '2010 Q2',
-            iphone: 2778,
-            ipad: 2294,
-            itouch: 2441
+            period: '2am',
+            RAM: 69,
+            CPU: 42
         }, {
-            period: '2010 Q3',
-            iphone: 4912,
-            ipad: 1969,
-            itouch: 2501
+            period: '4am',
+            RAM: 69,
+            CPU: 42
         }, {
-            period: '2010 Q4',
-            iphone: 3767,
-            ipad: 3597,
-            itouch: 5689
+            period: '6am',
+            RAM: 69,
+            CPU: 42
         }, {
-            period: '2011 Q1',
-            iphone: 6810,
-            ipad: 1914,
-            itouch: 2293
+            period: '8am',
+            RAM: 69,
+            CPU: 42
         }, {
-            period: '2011 Q2',
-            iphone: 5670,
-            ipad: 4293,
-            itouch: 1881
+            period: '10am',
+            RAM: 69,
+            CPU: 42
         }, {
-            period: '2011 Q3',
-            iphone: 4820,
-            ipad: 3795,
-            itouch: 1588
+            period: '12pm',
+            RAM: 69,
+            CPU: 42
         }, {
-            period: '2011 Q4',
-            iphone: 15073,
-            ipad: 5967,
-            itouch: 5175
+            period: '2pm',
+            RAM: 69,
+            CPU: 42
         }, {
-            period: '2012 Q1',
-            iphone: 10687,
-            ipad: 4460,
-            itouch: 2028
+            period: '4pm',
+            RAM: 69,
+            CPU: 42
         }, {
-            period: '2012 Q2',
-            iphone: 8432,
-            ipad: 5713,
-            itouch: 1791
+            period: '6pm',
+            RAM: 69,
+            CPU: 42
+        }, {
+            period: '8pm',
+            RAM: 69,
+            CPU: 42
+        }, {
+            period: '10pm',
+            RAM: 69,
+            CPU: 42
         }],
         xkey: 'period',
-        ykeys: ['iphone', 'ipad', 'itouch'],
-        labels: ['iPhone', 'iPad', 'iPod Touch'],
+        ykeys: ['RAM','CPU'],
+        labels: ['RAM', 'CPU'],
         pointSize: 2,
         hideHover: 'auto',
         resize: true
@@ -68,14 +66,11 @@ $(function() {
     Morris.Donut({
         element: 'morris-donut-chart',
         data: [{
-            label: "Download Sales",
+            label: "Discord Bot lists",
             value: 12
         }, {
-            label: "In-Store Sales",
+            label: "Direct From Bot",
             value: 30
-        }, {
-            label: "Mail-Order Sales",
-            value: 20
         }],
         resize: true
     });
