b3f42ca25a6f9305cf8179b1a65511baa7813617
Austin Melms <memyselfandlie@gmail.com>
2017-07-14 04:55:24 -0400

    fd

--- shard.js
+++ shard.js
@@ -3,7 +3,7 @@ const GetYourOwnToken = require('./config.json').token
 
 const shard = new Dickpunch.ShardingManager('./memedaddy.js', { token: GetYourOwnToken })
 
-shard.spawn(2)
+shard.spawn(13)
 
 shard.on('launch', shard => {
 	console.log(`Shard ${shard.id} is alive`)
