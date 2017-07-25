c06f9adc3b6bcedce819fa9d5a513a66099bff40
Aetheryx <zain1@live.nl>
2017-07-26 01:35:31 +0200

    Fixed bother, using raw for more shit now

--- commands/dm.js
+++ commands/dm.js
@@ -1,20 +1,17 @@
-exports.run = async function (client, msg, args, config, EmbedBuilder) {
+exports.run = async function (client, msg, args, utils) {
 	if (!config.devs.includes(msg.author.id)) return
-	const melmsie = client.users.get(config.owner)
 	try {
 		let user
 		if (client.users.get(args[0]))
 			user = client.users.get(args[0])
 		else
 			user = await client.fetchUser(args[0])
-		await user.send({
-			embed: new EmbedBuilder()
-				.setColor('#3676b3')
-				.setTitle('📫 You have received a message from the developer!')
-				.setAuthor('Melmsie', melmsie.avatarURL)
-				.setDescription(args.slice(1).join(' '))
-				.setFooter('To reply, please use pls bother')
-		})
+		await user.send({ embed: {
+			color: utils.colors.purple,
+			title: '📫 You have received a message from the developers!',
+			description: args.slice(1).join(' '),
+			footer: { text: 'To reply, please use pls bother' }
+		}})
 		await msg.react('📧')
 	} catch (e) {
 		await msg.react('❌')
--- commands/invite.js
+++ commands/invite.js
@@ -1,8 +1,9 @@
-exports.run = function (client, msg, args, config, EmbedBuilder) {
-	msg.channel.send({
-		embed: new EmbedBuilder()
-			.setColor('#3676b3')
-			.addField('Add Dank Memer', `\n[Here](https://discordapp.com/oauth2/authorize?client_id=${client.user.id}&scope=bot&permissions=8)`, true)
-			.addField('Join a Dank Server', '\n[Here](https://discord.gg/3GNMJBG)', true)
-	})
+exports.run = function (client, msg, args, utils) {
+	msg.channel.send({ embed: {
+		color: utils.colors.lightblue,
+		fields: [
+			{ name: 'Add Dank Memer', value: `\n[Here](https://discordapp.com/oauth2/authorize?client_id=${client.user.id}&scope=bot&permissions=8)`, inline: true },
+			{ name: 'Join a Dank Server', value: '\n[Here](https://discord.gg/3GNMJBG)', inline: true }
+		]
+	}})
 }
