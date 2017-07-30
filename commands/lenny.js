5a48efb6fcec486fb25710ebd2b5896f04756098
Austin Melms <memyselfandlie@gmail.com>
2017-07-30 12:41:30 -0400

    prep for today's update

--- commands/lenny.js
+++ commands/lenny.js
@@ -1,7 +1,3 @@
-exports.run = function (client, msg) {
-	if (!client.ids.donors.donor1.concat(client.ids.donors.donor5, client.ids.donors.donor10).includes(msg.author.id)) {
-		return msg.reply('to access this command, you must donate at any tier here: <https://www.patreon.com/melmsie>')
-	}
-
-	msg.channel.send('( ͡° ͜ʖ ͡° )')
+exports.run = async function (client, msg) {
+	await msg.channel.send('( ͡° ͜ʖ ͡° )')
 }
---
+++ tags.js
@@ -0,0 +1,30 @@
+module.exports = {
+	'doge': {
+		img: 'http://www.stickpng.com/assets/thumbs/5845e69dfb0b0755fa99d7ef.png',
+		info: 'Doge is an Internet meme that became popular in 2013. The meme typically consists of a picture of a Shiba Inu accompanied by multicolored text in Comic Sans font in the foreground. The text, representing a kind of internal monologue, is deliberately written in a form of broken English.'
+	},
+	'dolan': {
+		img: 'http://vignette1.wikia.nocookie.net/clubpenguinpookie/images/9/9f/Dolan.png',
+		info: 'Dolan is an MS Paint web comic series featuring a variety of poorly drawn Disney cartoon characters. The main character Dolan, based on Donald Duck, is often portrayed as a psychotic killer and sex addict.'
+	},
+	'kappa': {
+		img: 'https://vignette4.wikia.nocookie.net/kancolle/images/8/81/Kappa.png',
+		info: 'Kappa is a graphic emoticon commonly used by trolls as a postscript to a sentence to convey sarcasm on the live streaming video platform Twitch; it’s popularity has also lead to the emoticon being used as a form of spam.'
+	},
+	'lul': {
+		img: 'https://static-cdn.jtvnw.net/jtv_user_pictures/l_only_say_lul-profile_image-b558b7ec1d7e496e-300x300.png',
+		info: 'LUL is a BetterTTV (addon for Twitch.tv, a very popular streaming website) emote which is used in Twitch chat to express the same feelings as "lol"'
+	},
+	'megusta': {
+		img: 'https://vignette1.wikia.nocookie.net/crossoverrp/images/0/02/Me_gusta.png',
+		info: 'Me Gusta is a rage comic face that is typically used to express one’s approval of an awkward or perverse situation. The caption “Me gusta” translates to “I like it” in Spanish. In its beginning, the phrase conveyed an odd sense of pleasure in sexually perverse contexts, but the meaning has since broadened to describe a more general state of being disturbed and pleased at the same time.'
+	},
+	'pepe': {
+		img: 'http://vignette3.wikia.nocookie.net/cookietest/images/e/e2/FeelsBadMan.png',
+		info: 'Pepe the Frog is an anthropomorphic frog character from the comic series Boy’s Club by Matt Furie. On 4chan, various illustrations of the frog creature have been used as reaction faces, including Feels Good Man, Sad Frog, Angry Pepe, Smug Frog and Well Meme’d.'
+	},
+	'troll': {
+		img: 'http://www.stickpng.com/assets/images/580b585b2edbce24c47b2a27.png',
+		info: 'Trollface is a rage comic character wearing a mischievous smile that is meant to represent the facial expression of an Internet troll. The image is most commonly used to portray a character as a troll in rage comics, or alternatively, to identify oneself or another participant as such in online discussions.'
+	}
+}
