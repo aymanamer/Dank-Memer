a1107de8aee8afb2e7145259c2ea3189d5ce8770
Austin Melms <memyselfandlie@gmail.com>
2017-07-30 22:57:51 -0400

    better looking bitches

--- commands/pupper.js
+++ commands/pupper.js
@@ -3,15 +3,22 @@ exports.run = async function (client, msg) {
 	getDogPic(msg)
 }
 
-async function getDogPic (msg) {
+async function getDogPic(msg) {
 	const data = await snekfetch.get('https://random.dog/woof.json')
 	if (data.body.url.includes('.mp4')) {
 		return getDogPic(msg)
 	}
 
-	msg.channel.send({
-		files: [{
-			attachment: data.body.url
-		}]
+	await msg.channel.send({
+		embed: {
+			title: '🐶',
+			color: '5881576',
+			image: {
+				url: data.body.url
+			},
+			footer: {
+				text: `Requested by ${msg.author.tag}`
+			}
+		}
 	})
 }
