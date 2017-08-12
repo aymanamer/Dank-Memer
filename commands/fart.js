e3f379a505c87377c6aeb60ddfce2ad3428ae37a
Aetheryx <zain1@live.nl>
2017-08-12 02:17:47 +0200

    Attempted fix on voice commands #69420

--- commands/fart.js
+++ commands/fart.js
@@ -3,20 +3,20 @@ const file = Math.floor(Math.random() * 5 + 1)
 exports.run = async function (Memer, msg) {
 	if (!msg.member.voiceState.channelID) {
 		await msg.addReaction('❌')
-		return Memer.reply('join a voice channel fam', msg)
+		return Memer.reply('join a voice channel fam', msg);
 	}
 
 	if (!Memer.client.getChannel(msg.member.voiceState.channelID).permissionsOf(Memer.client.user.id).has('voiceConnect') ||
 		!Memer.client.getChannel(msg.member.voiceState.channelID).permissionsOf(Memer.client.user.id).has('voiceSpeak')) {
-		return Memer.reply('Well shit, there was a permission error! Make sure I have `connect` and `speak` so I can do this shit!', msg)
-	}
+		return Memer.reply('Well shit, there was a permission error! Make sure I have `connect` and `speak` so I can do this shit!', msg);
+	};
 
 	if (!Memer.client.voiceConnections.get(msg.channel.guild.id)) {
 		msg.addReaction('👍')
 		const conn = await Memer.client.joinVoiceChannel(msg.member.voiceState.channelID)
 		conn.play(`./assets/farts/${file}.mp3`)
 		conn.once('end', async () => {
-			await Memer.client.leaveVoiceChannel(msg.member.voiceState.channelID)
+			await Memer.client.leaveVoiceChannel(msg.channel.guild.members.get(Memer.client.user.id).voiceState.channelID)
 			if (Memer.client.voiceConnections.get(msg.channel.guild.id)) {
 				await Memer.client.voiceConnections.get(msg.channel.guild.id).disconnect()
 				await Memer.client.voiceConnections.get(msg.channel.guild.id)._destroy()
