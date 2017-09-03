21d2dcaf4674c07b7c078c282cfc4a1aca192f8c
Aetheryx <zain1@live.nl>
2017-09-03 02:14:02 +0200

    Add command loading and fixes

--- src/mainClass.js
+++ src/mainClass.js
@@ -1,53 +1,123 @@
 const utils = require('./utils/misc.js');
 const fs = require('fs');
-const cmdConfig = require('../cmdConfig.json');
+const msgHandler = require('./handlers/msgHandler.js');
+
 class Memer {
     constructor (bot) {
         this.bot = bot;
-    }
-
-    launch () {
         this.log = require('./utils/logger.js');
-        this.snek = require('snekfetch');
+        this._snek = require('snekfetch');
+        this._join = require('path').join;
+        this.config = require('./config.json');
         this.r = require('rethinkdbdash')();
-        this.db = require('./util/dbFunctions.js')(this.r);
+        this.db = require('./utils/dbFunctions.js')(this.r);
+        this.cmds = new Map();
+        this.aliases = new Map();
+        this.tags = new Map();
+
         for (const i in Object.keys(utils)) {
             this[Object.keys(utils)[i]] = utils[Object.keys(utils)[i]];
         }
-        this.bot
-            .on('ready', this.ready)
-            .on('guildCreate', this.guildCreate)
-            .on('guildDelete', this.guildDelete)
-            .on('messageCreate', this.messageCreate)
-            .on('error', this.onError);
     }
 
-    static ready () {
-        this.bot.client.editStatus(null, {
+    launch () {
+        this.bot
+            .on('ready', this.ready.bind(this))
+            .on('guildCreate', this.guildCreate.bind(this))
+            .on('guildDelete', this.guildDelete.bind(this))
+            .on('messageCreate', this.messageCreate.bind(this))
+            .on('error', this.onError.bind(this));
+
+        this.ready();
+        this.loadCommands();
+    }
+
+    ready () {
+        this.bot.editStatus(null, {
             name: 'pls help',
             type: 1,
             url: 'https://www.twitch.tv/teamzars'
         });
     }
 
-    static guildCreate (guild) {
+    loadCommands () {
+        const path = './commands';
+        fs.readdir(path, (error, files) => {
+            if (error) {
+                return this.log(error.stack, 'error');
+            }
 
+            files.forEach(file => {
+                if (file === 'temp') return
+                try {
+                    const command = require(this._join(__dirname, path, file));
+                    this.cmds.set(command.props.name, command);
+                    command.props.aliases.forEach(alias => {
+                        this.aliases.set(alias, command.props.name);
+                    });
+                } catch (error) {
+                    this.log(`Failed to load command ${file}:\n${error.stack}`, 'error');
+                }
+            });
+        });
+
+        const tags = require('./tags.json');
+        Object.keys(tags).forEach(tag => {
+            this.tags.set(tag, tags[tag]);
+        });
     }
 
-    static guildDelete (guild) {
+    guildCreate (guild) {
+        const embed = {
+            color: Memer.colors.lightblue,
+            title: 'Hello!',
+            description: Memer.intro
+        };
+        guild.channels.get(guild.channels.filter(c => c.type === 0).map(c => c.id)[0]).createMessage({ embed })
+            .catch(() => {});
+        // this.postStats();
+    }
+
+    guildDelete (guild) {
         this.db.deleteGuild(guild.id);
-        // postStats();
+        // this.postStats();
     }
 
-    static messageCreate (msg) {
-
+    get defaultGuildConfig () {
+        return {
+            prefix: this.config.defaultPrefix,
+            disabledCommands: []
+        };
     }
 
-    static onError (error) {
+    async messageCreate (msg) {
+        if (!msg.channel.guild ||
+        msg.author.bot/* ||
+        Memer.ids.blocked.user.includes(msg.author.id) || blocked will have to be moved to db because of multiprocess
+        Memer.ids.blocked.guild.includes(msg.channel.guild.id)*/
+    ) {
+            return;
+        }
 
+        this.log(this.config);
+
+        const gConfig = await this.db.getGuild(msg.channel.guild.id) || this.defaultGuildConfig;
+        
+        if (msg.mentions.find(m => m.id === this.bot.user.id) && msg.content.toLowerCase().includes('help')) {
+            return msg.channel.createMessage(`Hello, ${msg.author.username}. My prefix is \`${gConfig.prefix}\`. Example: \`${gConfig.prefix} meme\``);
+        }
+        this.log(gConfig.prefix + msg.content.toLowerCase())
+        if (msg.content.toLowerCase().startsWith(gConfig.prefix)) {
+            this.log('2')
+            msgHandler.handleMeDaddy(this, msg, gConfig);
+        }
     }
 
-    static async postStats () {
+    onError (error) {
+        this.log(error.stack, 'error');
+    }
+
+    async postStats () {
         const botlists = [
             ['https://bots.discord.pw/api/bots/270904126974590976/stats', Memer.config.pwtoken],
             ['https://discordbots.org/api/bots/270904126974590976/stats', Memer.config.orgtoken],
@@ -67,6 +137,10 @@ class Memer {
         const commands = fs.readdirSync('commands').join(' ').replace(/.js/g, '').split(' ');
         const aliases = Object.keys(cmdConfig.aliases);
         return commands.concat(aliases);
+    }
+
+    get cmdConfig () {
+        return cmdConfig;
     }
 
 
