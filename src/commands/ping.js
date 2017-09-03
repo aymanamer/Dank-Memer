09455dfe6ad97f094cce7e2957e6f034ec08bbbe
Aetheryx <zain1@live.nl>
2017-09-03 02:16:42 +0200

    Update to new syntax (test command)

--- src/commands/ping.js
+++ src/commands/ping.js
@@ -1,3 +1,11 @@
 exports.run = function (Memer, msg) {
-	msg.channel.createMessage(`${msg.channel.guild.shard.latency}ms`)
-}
+    msg.channel.createMessage(`${msg.channel.guild.shard.latency}ms`);
+};
+
+exports.props = {
+    name        : 'ping',
+    usage       : '{command}',
+    aliases     : ['hi'],
+    cooldown    : 1500,
+    description : 'hi'
+};
