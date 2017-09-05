d5dcafb5c7ebe58fddb4f4c36031560d978b378b
Aetheryx <zain1@live.nl>
2017-09-06 01:10:26 +0200

    Remove message collector

--- src/commands/tweet.js
+++ src/commands/tweet.js
@@ -21,24 +21,20 @@ exports.run = async function (Memer, msg, args) {
         return msg.channel.createMessage(`Tweet too long. You're ${args.join(' ').length - 140} characters over the limit!`);
     }
 
-    msg.channel.createMessage(`Are you sure you want to tweet \`${args.join(' ')}\`?\nYou will be **permanently banned** from using Dank Memer for tweets that are mean or racist. Currently banned: ${Memer.ids.blocked.user.length} idiots.\n\nAnswer with \`yes\`/\`no\`.`);
-
-    const [messages, reason] = await Memer.createMessageCollector(msg.channel, m => m.author.id === msg.author.id, { maxMatches: 1, time: 30000 });
-
-    if (reason === 'maxMatches' && messages[0].content.toLowerCase() === 'yes') {
-        tClient.post('statuses/update', { status: args.join(' ') }, (err, data, response) => {
-            if (err) {
-                return msg.channel.createMessage(`Something went wrong. \n${err.message}`);
-            }
-            if (response.statusCode !== 200) {
-                return msg.channel.createMessage('Something went wrong. Please try again later.');
-            }
-            msg.channel.createMessage({ embed: {
-                color: Memer.colors.lightblue,
-                title: 'Tweet Sent!',
-                description: `[View here](https://twitter.com/${data.user.screen_name}/status/${data.id_str})`,
-                footer: { text: 'See this tweet, and more @plsmeme' }
-            }});
+    tClient.post('statuses/update', { status: args.join(' ') }, (err, data, response) => {
+        if (err) {
+            return msg.channel.createMessage(`Something went wrong. \n${err.message}`);
+        }
+        if (response.statusCode !== 200) {
+            return msg.channel.createMessage('Something went wrong. Please try again later.');
+        }
+        msg.channel.createMessage({ embed: {
+            color: Memer.colors.lightblue,
+            title: 'Tweet Sent!',
+            description: `[View here](https://twitter.com/${data.user.screen_name}/status/${data.id_str})`,
+            footer: { text: 'See this tweet, and more @plsmeme' }
+        }});
+        try {
             Memer.bot.createMessage('326384964964974602', {
                 content: Memer.bannedWords.some(word => args.join(' ').toLowerCase().includes(word)) ? '<@&339186850910699520> BAD TWEET LADS WEE WOO WEE WOO' : '',
                 embed: {
@@ -51,14 +47,10 @@ exports.run = async function (Memer, msg, args) {
                     footer: { text: `Tweet ID: ${data.id_str} | Guild ID: ${msg.channel.guild.id} `},
                     timestamp: new Date(),
                 }});
-        });
-    } else if (reason === 'maxMatches' && messages[0].content.toLowerCase() === 'no') {
-        msg.channel.createMessage('Good. Watching you :eyes:');
-    } else if (reason === 'maxMatches') {
-        msg.channel.createMessage('mk bye then');
-    } else {
-        msg.channel.createMessage('Prompt timed out.');
-    }
+        } catch (e) {
+            Memer.log(e.stack, 'error');
+        }
+    });
 };
 
 exports.props = {
