a7d328e616df9811b1ebb465cb1b29987e00a7ff
Austin Melms <memyselfandlie@gmail.com>
2017-09-22 13:47:22 -0400

    Revert "Go back to hosted imgen"
    This reverts commit 4ec360ff037d31d2f0014cd847eb26bd983f4e64.

--- src/commands/warp.js
+++ src/commands/warp.js
@@ -5,7 +5,7 @@ exports.run = async function (Memer, msg, args) {
 	}
 
 	const data = await Memer._snek
-		.get('http://getame.me/api/warp')
+		.get('http://localhost/api/warp')
 		.set('Api-Key', Memer.config.imgenKey)
 		.set('data-src', avatarurl)
 
