7f8cabc17128a0b498868dd92c5b8fa694a26e62
Austin Melms <memyselfandlie@gmail.com>
2017-09-27 21:01:25 -0400

    STOP FARTING

--- src/commands/stop.js
+++ src/commands/stop.js
@@ -1,18 +1,18 @@
 exports.run = async function (Memer, msg) {
-	if (!Memer.client.voiceConnections.get(msg.channel.guild.id)) {
+	if (!Memer.bot.voiceConnections.get(msg.channel.guild.id)) {
 		return msg.channel.createMessage('I\'m not even playing anything in this server <:waitwhat:320387072290455554>')
 	}
 	if (!msg.member.voiceState.channelID) {
 		return msg.channel.createMessage('You\'re not even in a voice channel <:waitwhat:320387072290455554>')
 	}
-	if (msg.member.voiceState.channelID !== Memer.client.voiceConnections.get(msg.channel.guild.id).channelID) {
+	if (msg.member.voiceState.channelID !== Memer.bot.voiceConnections.get(msg.channel.guild.id).channelID) {
 		return msg.channel.createMessage('You\'re not even in my voice channel <:waitwhat:320387072290455554>')
 	}
-	await Memer.client.voiceConnections.get(msg.channel.guild.id).stopPlaying()
-	if (Memer.client.voiceConnections.get(msg.channel.guild.id)) {
-		await Memer.client.voiceConnections.get(msg.channel.guild.id).disconnect()
-		await Memer.client.voiceConnections.get(msg.channel.guild.id)._destroy()
-		await Memer.client.voiceConnections.remove(Memer.client.voiceConnections.get(msg.guild.id))
+	await Memer.bot.voiceConnections.get(msg.channel.guild.id).stopPlaying()
+	if (Memer.bot.voiceConnections.get(msg.channel.guild.id)) {
+		await Memer.bot.voiceConnections.get(msg.channel.guild.id).disconnect()
+		await Memer.bot.voiceConnections.get(msg.channel.guild.id)._destroy()
+		await Memer.bot.voiceConnections.remove(Memer.bot.voiceConnections.get(msg.guild.id))
 	}
 	msg.addReaction('❌')
 }
