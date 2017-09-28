0b770eeed89a573136d4f2d1eff4d7eca82d5f74
Austin Melms <memyselfandlie@gmail.com>
2017-09-27 20:25:47 -0400

    fart fix

--- src/commands/fart.js
+++ src/commands/fart.js
@@ -6,26 +6,26 @@ exports.run = async function (Memer, msg) {
 		return Memer.reply('join a voice channel fam', msg)
 	}
 
-	if (!Memer.client.getChannel(msg.member.voiceState.channelID).permissionsOf(Memer.client.user.id).has('voiceConnect') ||
-		!Memer.client.getChannel(msg.member.voiceState.channelID).permissionsOf(Memer.client.user.id).has('voiceSpeak')) {
-		return Memer.reply('Well shit, there was a permission error! Make sure I have `connect` and `speak` so I can do this shit!', msg)
+	if (!Memer.bot.getChannel(msg.member.voiceState.channelID).permissionsOf(Memer.bot.user.id).has('voiceConnect') ||
+		!Memer.bot.getChannel(msg.member.voiceState.channelID).permissionsOf(Memer.bot.user.id).has('voiceSpeak')) {
+		return msg.reply('Well shit, there was a permission error! Make sure I have `connect` and `speak` so I can do this shit!', msg)
 	}
 
-	if (!Memer.client.voiceConnections.get(msg.channel.guild.id)) {
+	if (!Memer.bot.voiceConnections.get(msg.channel.guild.id)) {
 		msg.addReaction('👍')
-		const conn = await Memer.client.joinVoiceChannel(msg.member.voiceState.channelID)
+		const conn = await Memer.bot.joinVoiceChannel(msg.member.voiceState.channelID)
 		conn.play(`./assets/farts/${file}.mp3`)
 		conn.once('end', async () => {
-			await Memer.client.leaveVoiceChannel(msg.channel.guild.members.get(Memer.client.user.id).voiceState.channelID)
-			if (Memer.client.voiceConnections.get(msg.channel.guild.id)) {
-				await Memer.client.voiceConnections.get(msg.channel.guild.id).disconnect()
-				await Memer.client.voiceConnections.get(msg.channel.guild.id)._destroy()
-				await Memer.client.voiceConnections.remove(Memer.client.voiceConnections.get(msg.guild.id))
+			await Memer.bot.leaveVoiceChannel(msg.channel.guild.members.get(Memer.bot.user.id).voiceState.channelID)
+			if (Memer.bot.voiceConnections.get(msg.channel.guild.id)) {
+				await Memer.bot.voiceConnections.get(msg.channel.guild.id).disconnect()
+				await Memer.bot.voiceConnections.get(msg.channel.guild.id)._destroy()
+				await Memer.bot.voiceConnections.remove(Memer.bot.voiceConnections.get(msg.guild.id))
 			}
 		})
 	} else {
 		await msg.addReaction('❌')
-		Memer.reply('I only have one butthole, dude. Please wait until the current sound is done or the ear-rape ghost will visit you in your sleep!', msg)
+		msg.reply('I only have one butthole, dude. Please wait until the current sound is done or the ear-rape ghost will visit you in your sleep!', msg)
 	}
 }
 
