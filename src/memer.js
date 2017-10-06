c3ff957644f22b43d91b9f859c2ac80728412a57
Austin Melms <memyselfandlie@gmail.com>
2017-10-06 05:18:24 -0400

    reposting to botlists

--- src/memer.js
+++ src/memer.js
@@ -7,7 +7,7 @@ metrics.init({
   prefix: 'dank.'
 })
 const Sharder = require('eris-sharder').Master
-// const snek = require('snekfetch')
+const snek = require('snekfetch')
 const r = require('rethinkdbdash')()
 const master = new Sharder(config.token, '/mainClass.js', {
   name: 'Dank Memer',
@@ -16,13 +16,13 @@ const master = new Sharder(config.token, '/mainClass.js', {
   clientOptions: config.clientOptions
 })
 
-/*
+
 const botlists = new Map([
   ['https://bots.discord.pw/api/bots/270904126974590976/stats', config.pwtoken],
   ['https://discordbots.org/api/bots/270904126974590976/stats', config.orgtoken],
   ['https://www.carbonitex.net/discord/data/botdata.php', config.carbon]
 ])
-*/
+
 master.on('stats', res => {
   metrics.gauge('totalGuilds', res.guilds)
   metrics.gauge('totalRam', res.totalRam)
@@ -30,7 +30,7 @@ master.on('stats', res => {
   r.table('stats')
     .insert({ id: 1, stats: res }, { conflict: 'update' })
     .run()
-  /*
+  
   botlists.forEach(async (token, url) => {
     await snek
       .post(url)
@@ -41,5 +41,5 @@ master.on('stats', res => {
       })
       .end()
   })
-  */
+
 })
