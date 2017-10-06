06b8ea30a8d8cdd266c5a2df58f5f8374336d3e9
Austin Melms <memyselfandlie@gmail.com>
2017-10-06 05:32:33 -0400

    finally fixed

--- src/commands/henlo.js
+++ src/commands/henlo.js
@@ -5,22 +5,18 @@ exports.run = async function (Memer, msg, args) {
 	let mention
 	let text
 	if (!args[0]) {
-		console.log('1')
 		mention = msg.author.username
 		text = Memer.randomInArray(henlo)
 	} else if (msg.mentions.length > 0) {
-		console.log('2')
 		mention = msg.mentions[0].username
 		text = args[1] ? args[1] : Memer.randomInArray(henlo)
 	} else if (msg.mentions.length === 0) {
-		console.log('3')
 		mention = msg.author.username
 		text = args[0] ? args[0] : Memer.randomInArray(henlo)
 	} else {
-		console.log('4')
 		return msg.create('hmm, you seemed to find a way to break this. GG at beating my 5am brain.')
 	}
-	console.log('5')
+
 	msg.channel.createMessage(`henlo ${mention}\nyou stinky ${mention}\ngo ${text} ugly`)
 }
 
