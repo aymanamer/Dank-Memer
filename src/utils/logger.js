24eacf579da779e3066f8c0f66d9d2bd3fe6d88a
Aetheryx <zain1@live.nl>
2017-10-23 01:02:59 +0200

    Update logger to new parameter name

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -1,6 +1,6 @@
-function logger (message, type = 'log') {
+function logger (message, name = 'log') {
   const date = Date().toString().split(' ').slice(1, 5).join(' ')
-  process.send({ type, msg: `[${date}] ${message}` })
+  process.send({ name, msg: `[${date}] ${message}` })
 }
 global.console.error = (str) => { logger(`Rethink log: ${str}`, 'warn') } // rethinkdb automatically sends any information to console.error
 module.exports = logger
