bbe575203d66159464d736de3509050c4ef465e9
Aetheryx <zain1@live.nl>
2017-10-31 12:19:02 +0100

    Update trigger.js

--- src/commands/trigger.js
+++ src/commands/trigger.js
@@ -2,6 +2,7 @@ const GenericImageCommand = require('../models/GenericImageCommand.js')
 
 const command = new GenericImageCommand('trigger', null, {
   aliases: ['triggered'],
+  format: 'gif',
   description: 'UR SO TRIGGERED BRO'
 })
 
