e597ee677abb2a96dcdc703fcbe6f436600d06f7
Aetheryx <zain1@live.nl>
2017-11-01 18:51:48 +0100

    Remove upvote lock

--- src/commands/porn.js
+++ src/commands/porn.js
@@ -1,22 +1,12 @@
 
 exports.run = async function (Memer, msg) {
-  const votes = await Memer._snek.get('https://discordbots.org/api/bots/270904126974590976/votes?onlyids=1').set('Authorization', Memer.config.orgtoken)
-  if (!votes.body.includes(msg.author.id)) {
-    msg.channel.createMessage({ embed: {
-      color: Memer.colors.lightblue,
-      author: { name: 'OWO whats this?' },
-      thumbnail: { url: 'http://icons.iconarchive.com/icons/paomedia/small-n-flat/1024/lock-icon.png' },
-      description: 'To be able to see this **NSFW content**, you must vote.\n\n[Click here to vote.](https://discordbots.org/bot/memes)',
-      footer: { text: 'Locked Command' }
-    }})
-  } else {
-    msg.channel.createMessage({ embed: {
-      color: Memer.colors.lightblue,
-      author: { name: 'Nice meme.' },
-      description: '[Click here to get a REAL porn bot](https://bot.discord.io/boobbot). Thanks for upvoting though!',
-      footer: { text: 'Yes, boobbot is holding me hostage. Shhhhh' }
-    }})
-  }
+  msg.channel.createMessage({ embed: {
+    color: Memer.colors.lightblue,
+    author: { name: 'BoobBot Approves', icon_url: 'https://cdn.discordapp.com/app-icons/285480424904327179/3b0ddd147a565307705b6735c45ef448.jpg' },
+    title: 'Do I look like a fucking porn bot to you? Fucking click these.',
+    description: '<:boobblob:375031062000304129> Server: [Click here](https://discord.gg/SPArpWN)\n<:boobblob:375031062000304129> Bot: [Click here](https://bot.discord.io/boobbot "Pictures of nature. ❤")',
+    footer: { text: 'Yes, boobbot is holding me hostage. Shhhhh' }
+  }})
 }
 
 exports.props = {
