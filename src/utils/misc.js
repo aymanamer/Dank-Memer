fd027cdd36486a4e0f0f7c9d6b92dcf4b4238cee
Aetheryx <zain1@live.nl>
2017-11-05 23:17:10 +0100

    Replace moment-duration-format with custom function

--- src/utils/misc.js
+++ src/utils/misc.js
@@ -1,6 +1,4 @@
 const config = require('../config.json')
-const moment = require('moment')
-require('moment-duration-format')
 
 module.exports = {
   colors: {
@@ -45,7 +43,12 @@ module.exports = {
     return `${'```'}${lang || ''}\n${str}\n${'```'}`
   },
 
-  parseTime: (time, format) => {
-    return moment.duration(parseInt(time), format || 'seconds').format('dd:hh:mm:ss')
+  parseTime: (time) => {
+    const methods = [86400, 3600, 60, 1]
+    const timeStr = [Math.floor(time / methods[0]).toString().padStart(2, '0')]
+    for (let i = 0; i < 3; i++) {
+      timeStr.push(Math.floor(time % methods[i] / methods[i + 1]).toString().padStart(2, '0'))
+    }
+    return timeStr.join(':')
   }
 }
