06f1404ab3eb4cd41bfc96056a3bcbbc4d420a71
Austin Melms <memyselfandlie@gmail.com>
2017-11-08 05:04:56 -0500

    travis is happy NOW?

--- src/commands/call.js
+++ src/commands/call.js
@@ -1,12 +1,11 @@
 exports.run = function (Memer, msg, args) {
-
   msg.channel.createMessage({
     embed: {
 	  color: Memer.colors.lightblue,
-	  author: { name: 'Call Dank Memer!' },
-	  description: 'Dank Memer went and got himself a phone number. He\'d love to text you/hear any voicemails you have for him. Call or text him at `(765) 753-2191`.',
-	  footer: { text: 'If you call, he will not answer. Leave a voicemail.' }
-	  }
+      author: { name: 'Call Dank Memer!' },
+      description: 'Dank Memer went and got himself a phone number. He\'d love to text you/hear any voicemails you have for him. Call or text him at `(765) 753-2191`.',
+      footer: { text: 'If you call, he will not answer. Leave a voicemail.' }
+      }
   })
 }
 
--- src/handlers/msgHandler.js
+++ src/handlers/msgHandler.js
@@ -20,7 +20,7 @@ exports.handleMeDaddy = async function (Memer, msg, gConfig) {
     }
     return
   }
-  
+
   Memer.metrics.increment(`command-${command}`)
 
   if (!command.run || gConfig.disabledCommands.includes(command.props.name)) {
