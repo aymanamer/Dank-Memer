0e6232b126bf223da6d08350351c808d8931d144
Austin Melms <memyselfandlie@gmail.com>
2017-11-16 12:30:30 -0500

    k thanks then

--- src/commands/rust.js
+++ src/commands/rust.js
@@ -3,12 +3,12 @@ exports.run = async function (Memer, msg) {
   msg.channel.createMessage({
     embed: {
       color: Memer.colors.lightblue,
-      author: { name: 'Come Play Rust!'},
-      thumbnail: { url: 'https://cdn.discordapp.com/attachments/357084379371012099/379462443254022144/memerust.png'},
+      author: { name: 'Come Play Rust!' },
+      thumbnail: { url: 'https://cdn.discordapp.com/attachments/357084379371012099/379462443254022144/memerust.png' },
       title: 'Dank Memer\'s Rust Server!',
       description: 'Come play rust with fellow dank memers!',
       footer: { text: 'Wipes monthly.' },
-      image: { url: 'https://i.imgur.com/vwhaJ0D.png'}
+      image: { url: 'https://i.imgur.com/vwhaJ0D.png' }
     }
   })
 }
