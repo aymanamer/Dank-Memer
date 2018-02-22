2501d8c54a40561878b2eea4eb388612f5abcf73
Austin Melms <memyselfandlie@gmail.com>
2018-02-22 15:39:36 -0500

    made donate command even more of a meme

--- src/commands/patreon.js
+++ src/commands/patreon.js
@@ -4,14 +4,14 @@ exports.run = async function (Memer, msg) {
     description: 'Help Melmsie keep the bot alive by donating to pay server costs!',
     url: 'https://www.patreon.com/melmsie',
     color: Memer.randomColor(),
-    footer: { text: 'Donating will always be optional, and always appreciated! :D' }
+    footer: { text: '"pls donut for my bot, im running low on money"' }
   }})
 }
 
 exports.props = {
   name: 'patreon',
   usage: '{command}',
-  aliases: ['donate', 'gibmonies', 'pay'],
+  aliases: ['donate', 'gibmonies', 'pay', 'donut'],
   cooldown: 1000,
   description: 'See how you can donate to the bot and gain access to donor features!',
   perms: ['embedLinks']
