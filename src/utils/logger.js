ba7e1a6194103eb05bc4dac9b9b300786770035b
aetheryx <zain1@live.nl>
2018-02-26 00:59:45 +0100

    add util.inspect to logger

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -1,5 +1,6 @@
 function logger (message, name = 'log') {
   const date = Date().toString().split(' ').slice(1, 5).join(' ')
+  message = message instanceof Object ? require('util').inspect(message) : message
   process.send({ name, msg: `[${date}] ${message}` })
 }
 global.console.error = (str) => { logger(`Rethink log: ${str}`, 'warn') } // rethinkdb automatically sends any information to console.error
