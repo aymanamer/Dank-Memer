de146d3004ffb5b9cd47e4187e51f3bb61c25879
aetheryx <zain1@live.nl>
2018-02-26 00:59:28 +0100

    improve stat posting

--- src/memer.js
+++ src/memer.js
@@ -1,36 +1,35 @@
 const config = require('./config.json')
-
-const Sharder = require('eris-sharder').Master
-const snek = require('snekfetch')
+const { Master: Sharder } = require('eris-sharder')
+const { post } = require('snekfetch')
 const r = require('rethinkdbdash')()
 
 const master = new Sharder(config.token, '/mainClass.js', {
-  name: 'Dank Memer',
   stats: true,
   webhooks: config.webhooks,
   clientOptions: config.clientOptions,
-  shards: 96
+  shards: 1
 })
-
-const botlists = new Map([
-  ['https://bots.discord.pw/api/bots/270904126974590976/stats', config.pwtoken],
-  ['https://www.carbonitex.net/discord/data/botdata.php', config.carbon],
-  ['https://discordbots.org/api/bots/270904126974590976/stats', config.orgtoken]
-])
 
 master.on('stats', res => {
   r.table('stats')
     .insert({ id: 1, stats: res }, { conflict: 'update' })
     .run()
-
-  botlists.forEach(async (token, url) => {
-    snek
-      .post(url)
-      .set('Authorization', token)
-      .send({
-        [`server${url.includes('carbonitex') ? '' : '_'}count`]: res.guilds, // matt plz
-        key: token
-      })
-      .end()
-  })
 })
+
+if (require('cluster').isMaster && !config.dev) {
+  setInterval(async () => {
+    const { stats: { guilds } } = await r.table('stats')
+      .get(1)
+      .run()
+
+    for (const botlist of config.botlists) {
+      post(botlist.url)
+        .set('Authorization', botlist.token)
+        .send({
+          [`server${botlist.url.includes('carbonitex') ? '' : '_'}count`]: guilds, // matt plz
+          key: botlist.token
+        })
+        .end()
+    }
+  }, 5 * 60 * 1000)
+}
