5a680838d72d5f57899229f2c402308992e844a2
aetheryx <zain1@live.nl>
2018-02-27 21:18:39 +0100

    improvements to gamble

--- src/commands/gamble.js
+++ src/commands/gamble.js
@@ -2,34 +2,34 @@ const { GenericCommand } = require('../models/')
 
 module.exports = new GenericCommand(
   async ({ Memer, msg, args, addCD }) => {
-    const chances = Math.floor(Math.random() * 10) + 1
-    const numOne = Math.floor(Math.random() * 1) + 1
-    const numTwo = Math.floor(Math.random() * 5) * 0.1
-    const punishment = (Math.floor(Math.random() * 6) + 1.2) * 0.1
-    const bet = parseInt(args[0])
+    const bet = Number(args[0])
+    if (!bet) {
+      return { description: 'you have to bet actual coins, dont try to break me' }
+    }
+    if (bet < 1) {
+      return { description: 'u can\'t bet less than 1 coins you dumb' }
+    }
+
     let coins = await Memer.db.getCoins(msg.author.id)
-    if (isNaN(bet)) {
-      return {description: `you have to bet actual coins, dont try to break me`}
-    }
     if (bet > coins.coin) {
-      return {description: `u only have ${coins.coin} coins, dont bluff me`}
+      return { description: `u only have ${coins.coin} coins, dont bluff me` }
     }
-    if (bet < 0) {
-      return {description: `u can't bet 0 coins you dumb`}
-    }
+
     await addCD()
-    if (chances > 6) {
-      const winnings = Math.round(bet * (numOne + numTwo))
-      await Memer.db.addCoins(msg.author.id, winnings)
-      return {description: `u won ${winnings} coins, now u got ${coins.coin + parseInt(winnings)}`}
-    } else {
-      const losings = bet * punishment
-      const lost = Math.round(losings) + bet
-      if (coins.coin - lost <= 0) {
-        await Memer.db.removeCoins(msg.author.id, 0)
+
+    if (Math.random() >= 0.6) {
+      const multiplier = (Math.random() * 0.9) + 1
+
+      const winnings = Math.round(bet * multiplier)
+      if (winnings === coins.coin) {
+        return 'broke even'
       }
-      await Memer.db.removeCoins(msg.author.id, lost)
-      return {description: `haha u suck. u lost ${lost} coins. now u got ${coins.coin - lost}`}
+
+      await Memer.db.addCoins(msg.author.id, winnings)
+      return { description: `u won ${winnings} coins, now u got ${coins.coin + parseInt(winnings)}` }
+    } else {
+      await Memer.db.removeCoins(msg.author.id, bet)
+      return { description: `haha u suck. u lost ${bet} coins. now u got ${(coins.coin - bet) < 0 ? 0 : coins.coin - bet}` }
     }
   },
   {
