eae60950e5afa170ec3c6df31ad094c7888d1e01
Austin Melms <memyselfandlie@gmail.com>
2018-03-04 11:03:25 -0500

    rick roll command

---
+++ src/commands/freenitro.js
@@ -0,0 +1,14 @@
+const { GenericCommand } = require('../models/')
+
+module.exports = new GenericCommand(
+  () => {
+    return {
+      description: `I gotchu fam, have some [free discord nitro](https://goo.gl/CWqeBF)`,
+      footer: {text: 'you\'re welcome'}
+    }
+  },
+  {
+    triggers: ['freenitro', 'rickroll'],
+    description: 'owo whats this'
+  }
+)
