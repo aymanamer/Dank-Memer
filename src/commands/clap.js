061c58030878a25fe03fae157bbb156c43393a73
Austin Melms <memyselfandlie@gmail.com>
2018-03-04 11:01:05 -0500

    new text commands

---
+++ src/commands/clap.js
@@ -0,0 +1,12 @@
+const { GenericCommand } = require('../models/')
+
+module.exports = new GenericCommand(
+  async ({ args }) => args.join(' 👏 '),
+  {
+    triggers: ['clap'],
+    description: 'Make the bot say whatever you want with sass!',
+    usage: '{command} <what you want the bot to say>',
+
+    missingArgs: 'What do you want me to say?'
+  }
+)
