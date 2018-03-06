cca23dfd256ecadf75c6f4b55c6c8ba3422e1ad3
aetheryx <zain1@live.nl>
2018-03-06 23:03:56 +0100

    automatically build help from categories

--- src/commands/utilityCommands/help.js
+++ src/commands/utilityCommands/help.js
@@ -2,6 +2,28 @@ const { GenericCommand } = require('../../models')
 
 module.exports = new GenericCommand(
   async ({ Memer, msg, args, addCD }) => {
+    if (!args[0]) {
+      let categories = {}
+      for (const command of Memer.cmds) {
+        if (command.props.ownerOnly) {
+          continue
+        }
+
+        let category = categories[command.category]
+        if (!category) {
+          category = categories[command.category] = []
+        }
+        category.push(command.props.triggers[0])
+      }
+
+      return {
+        title: 'Available Commands',
+        description: 'Do pls changes to see what\'s in the new update!',
+        fields: Object.keys(categories).map(category => ({ name: category, value: categories[category].join(', ') })),
+        footer: { text: 'Hello darkness my old friend...' }
+      }
+    }
+
     const command = Memer.cmds.find(c => c.props.triggers.includes(args[0]))
     if (!command) {
       return
@@ -21,23 +43,6 @@ module.exports = new GenericCommand(
   {
     triggers: ['help', 'cmds', 'commands'],
     description: 'See a list of commands available.',
-    perms: ['embedLinks'],
-
-    missingArgs: {
-      title: 'Available Commands',
-      description: 'Do pls changes to see what\'s in the new update!',
-      fields: [
-        { name: `😂 Memey Commands`, value: 'discordmeme, wholesome, joke, me_irl, meme, memegen, prequel, pun, shitpost' },
-        { name: `🤣 Fun Commands`, value: 'asktrump, chucknorris, comic, dankrate, facepalm, freenitro, gayrate, google, greentext, kill, mock, ratewaifu, roast, say, showerthoughts, vent, xkcd' },
-        { name: '📷 Image Manipulation', value: 'b1nzy, ban, batslap, brazzers, byemom, cancer, cry, delete, egg, hitler, jail, magik, pride, rip, salty, search, shit, spank, trigger, tweet, warp' },
-        { name: '🐕 Animal Commands', value: 'aww, birb, kitty, lizzyboi, pupper, redpanda' },
-        { name: '🔊 Voice Commands', value: 'airhorn, boo, fart, knock, mememusic, mlgmusic, oof' },
-        { name: '😏 NSFW Commands', value: '4k, boobies, booty, gayporn, porngif' },
-        { name: '🆗 Text Commands', value: 'clap, emojify' },
-        { name: '💰 Currency Commands', value: 'coins, daily' },
-        { name: '🔧 Utilities and Information', value: 'changes, clean, credits, disable, enable, help, invite, patreon, prefix, stats, website' }
-      ],
-      footer: { text: 'Hello darkness my old friend...' }
-    }
+    perms: ['embedLinks']
   }
 )
