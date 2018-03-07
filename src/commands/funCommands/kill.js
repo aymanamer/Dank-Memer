acff1607c598b60bd2bfe28ce7d64bfa635c1d10
Luke <dev@crimsonxv.pro>
2018-03-07 05:39:56 +0000

    Update kill.js

--- src/commands/funCommands/kill.js
+++ src/commands/funCommands/kill.js
@@ -3,11 +3,8 @@ const { GenericCommand } = require('../../models/')
 
 module.exports = new GenericCommand(
   async ({ Memer, msg, args, addCD }) => {
-    if (args[0] === 'me' || msg.mentions[0].id === msg.author.id) {
+    if (args[0] === 'me' || !msg.mentions[0] || msg.mentions[0].id === msg.author.id) {
       return ' Ok you\'re dead. Please tag someone else to kill.'
-    }
-    if (!msg.mentions[0]) {
-      return 'Please tag someone to kill.'
     }
 
     return Memer.randomInArray(kill)
